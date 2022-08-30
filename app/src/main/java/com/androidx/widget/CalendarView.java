b7d98105dbf57366642ff9e8dd592befb4c96bed
RelinRan <1101005304@qq.com>
2022-08-30 12:58:56 +0800

    新增nextMonth()和lastMonth()方法; 修复月份31日bug；

--- app/src/main/java/com/androidx/widget/CalendarView.java
+++ app/src/main/java/com/androidx/widget/CalendarView.java
@@ -8,7 +8,6 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
